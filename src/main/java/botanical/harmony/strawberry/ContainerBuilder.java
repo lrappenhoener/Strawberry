3766850d360ec579d981c43024a95dc635622e85
lr <lr@lr.de>
2024-02-23 12:21:42 +0100

    Pass registered types when creating container

--- src/main/java/botanical/harmony/strawberry/ContainerBuilder.java
+++ src/main/java/botanical/harmony/strawberry/ContainerBuilder.java
@@ -1,16 +1,21 @@
 package botanical.harmony.strawberry;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ContainerBuilder {
+  private List<Class<?>> registeredTypes = new ArrayList<>();
 
   public static ContainerBuilder create() {
     return new ContainerBuilder();
   }
 
   public <T> void register(Class<T> class1) {
+    registeredTypes.add(class1);
   }
 
   public Container build() {
-    return new Container();
+    return new Container(registeredTypes);
   }
 
 }
