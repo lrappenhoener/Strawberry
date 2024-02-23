f0d344e25e920b4d1d8dc364b924e90b7519e541
lr <lr@lr.de>
2024-02-23 12:20:57 +0100

    Add check if type registered before resolve

--- src/main/java/botanical/harmony/strawberry/Container.java
+++ src/main/java/botanical/harmony/strawberry/Container.java
@@ -1,11 +1,20 @@
 package botanical.harmony.strawberry;
 
+import java.util.List;
 import java.util.Optional;
 import java.lang.reflect.Constructor;
 
 public class Container {
 
+  private List<Class<?>> registeredTypes;
+
+  public Container(List<Class<?>> registeredTypes) {
+    this.registeredTypes = registeredTypes;
+  }
+
   public <T> Optional<T> resolve(Class<T> class1) {
+    if (!registeredTypes.contains(class1))
+      return Optional.empty();
     try {
       Constructor<T> ctor = class1.getDeclaredConstructor();
       return Optional.of((T) ctor.newInstance());
