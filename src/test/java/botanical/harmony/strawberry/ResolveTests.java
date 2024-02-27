d63e0c9545ff34cfe22f635fd2ea2b664b7fe87a
lr <lr@lr.de>
2024-02-27 09:52:30 +0100

    Add resolving by factory test

--- src/test/java/botanical/harmony/strawberry/ResolveTests.java
+++ src/test/java/botanical/harmony/strawberry/ResolveTests.java
@@ -43,16 +43,49 @@ public class ResolveTests {
 
   @Test
   void resolve_type_with_dependencies() {
-    ContainerBuilder builder = ContainerBuilder.create();
-    builder.register(TestTypeWithDependencies.class);
-    builder.register(AnotherTestTypeWithDependencies.class);
-    builder.register(SimpleTestType.class);
+    ContainerBuilder builder = getContainerBuilder();
     Container container = builder.build();
 
     Optional<TestTypeWithDependencies> result = container.resolve(TestTypeWithDependencies.class);
 
     assertTrue(result.isPresent());
     assertSame(result.get().getClass(), TestTypeWithDependencies.class);
+  }
+
+  @Test
+  void resolve_type_with_dependencies_by_factory() {
+    ContainerBuilder builder = getContainerBuilder();
+    builder.register(TestTypeForFactory.class, (c) -> {
+      Optional<AnotherTestTypeWithDependencies> dependencyA = c.resolve(AnotherTestTypeWithDependencies.class);
+      Optional<SimpleTestType> dependencyB = c.resolve(SimpleTestType.class);
+      Optional<TestTypeWithDependencies> dependencyC = c.resolve(TestTypeWithDependencies.class);
+      return new TestTypeForFactory(dependencyA.get(), dependencyB.get(), dependencyC.get());
+    });
+    Container container = builder.build();
+
+    Optional<TestTypeForFactory> result = container.resolve(TestTypeForFactory.class);
+
+    assertTrue(result.isPresent());
+  }
+
+  private static ContainerBuilder getContainerBuilder() {
+    ContainerBuilder builder = ContainerBuilder.create();
+    builder.register(TestTypeWithDependencies.class);
+    builder.register(AnotherTestTypeWithDependencies.class);
+    builder.register(SimpleTestType.class);
+    return builder;
+  }
+}
+
+class TestTypeForFactory {
+  private final AnotherTestTypeWithDependencies dependencyA;
+  private final SimpleTestType dependencyB;
+  private final TestTypeWithDependencies dependencyC;
+
+  public TestTypeForFactory(AnotherTestTypeWithDependencies dependencyA, SimpleTestType dependencyB, TestTypeWithDependencies dependencyC) {
+    this.dependencyA = dependencyA;
+    this.dependencyB = dependencyB;
+    this.dependencyC = dependencyC;
   }
 }
 
