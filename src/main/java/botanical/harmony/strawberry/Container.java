36441f971f5751087d58cb037717e5a2cc94e6cf
lr <lr@lr.de>
2024-02-27 09:52:22 +0100

    Add resolving by factory

--- src/main/java/botanical/harmony/strawberry/Container.java
+++ src/main/java/botanical/harmony/strawberry/Container.java
@@ -3,20 +3,37 @@ package botanical.harmony.strawberry;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Parameter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.function.Function;
 
 public class Container {
 
   private final List<Class<?>> registeredTypes;
+  private final HashMap<Class<?>, List<Function<Container, ?>>> registeredTypeFactories;
 
-  public Container(List<Class<?>> registeredTypes) {
+  public Container(List<Class<?>> registeredTypes, HashMap<Class<?>, List<Function<Container, ?>>> registeredTypeFactories) {
     this.registeredTypes = registeredTypes;
+    this.registeredTypeFactories = registeredTypeFactories;
   }
 
   public <T> Optional<T> resolve(Class<T> clazz) {
+    Optional<T> instance = resolveByFactory(clazz);
+    if (instance.isPresent()) return instance;
+    return resolveByReflection(clazz);
+  }
+
+  private <T> Optional<T> resolveByFactory(Class<T> clazz) {
+    if (!registeredTypeFactories.containsKey(clazz))
+      return Optional.empty();
+    Optional<Function<Container, ?>> optionalFactory = registeredTypeFactories.get(clazz).stream().findFirst();
+    if (optionalFactory.isEmpty())
+      return Optional.empty();
+    Function<Container, ?> factory = optionalFactory.get();
+    Object instance = factory.apply(this);
+    return Optional.of((T)instance);
+  }
+
+  private <T> Optional<T> resolveByReflection(Class<T> clazz) {
     if (!registeredTypes.contains(clazz))
       return Optional.empty();
 
--- src/main/java/botanical/harmony/strawberry/ContainerBuilder.java
+++ src/main/java/botanical/harmony/strawberry/ContainerBuilder.java
@@ -1,10 +1,13 @@
 package botanical.harmony.strawberry;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.function.Function;
 
 public class ContainerBuilder {
-  private List<Class<?>> registeredTypes = new ArrayList<>();
+  private final List<Class<?>> registeredTypes = new ArrayList<>();
+  private final HashMap<Class<?>, List<Function<Container, ?>>> registeredFactories = new HashMap<>();
 
   public static ContainerBuilder create() {
     return new ContainerBuilder();
@@ -13,9 +16,21 @@ public class ContainerBuilder {
   public <T> void register(Class<T> class1) {
     registeredTypes.add(class1);
   }
+  public <T> void register(Class<T> clazz, Function<Container, T> factory) {
+    List<Function<Container, ?>> factories;
+    if (registeredFactories.containsKey(clazz)) {
+      factories = registeredFactories.get(clazz);
+      factories.add(factory);
+    }
+    else {
+      factories = new ArrayList<>();
+      factories.add(factory);
+      registeredFactories.put(clazz, factories);
+    }
+  }
 
   public Container build() {
-    return new Container(registeredTypes);
+    return new Container(registeredTypes, registeredFactories);
   }
 
 }
